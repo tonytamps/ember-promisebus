56ecb73a6cf166d6574d26e0416c0bb74955299f
Tony Tamplin <tony.t@franq.co>
2015-04-29 20:28:34 +1000

    Ensure lodash-includes are loaded by the consuming app.

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-promisebus'
+  name: 'ember-promisebus',
+  included: function(app) {
+    this._super.included(app);
+
+    app.import({
+      development: 'vendor/lodash-includes/lodash-includes.js',
+      production: 'vendor/lodash-includes/lodash-includes.min.js'
+    });
+  }
 };
